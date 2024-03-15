e20725fc3d26667db459c2eb35afa8672a345ab8
wikim <wiktormalyska03@gmail.com>
2024-03-15 01:54:09 +0100

    added ending nodes, colors, hidden physics settings, added some settings to make it look better

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ class Edge:
 
 
 class Node:
-    def __init__(self, cl, cr, ml, mr, boat):
+    def __init__(self, cl, cr, ml, mr, boat, color, is_end):
         self.cl = cl
         self.cr = cr
         self.ml = ml
         self.mr = mr
         self.boat: bool = boat
         self.index = self.get_index()
+        self.color: str = color
+        self.is_end: bool = is_end
 
     def get_index(self):
         boat = 1 if self.boat else 0
@@ -59,8 +61,8 @@ class Graph:
     def process_queue(self):
         while self.queue:
             node = self.queue.popleft()
-            self.check_connections(node)
-
+            if not node.is_end:
+                self.check_connections(node)
     def check_connections(self, node: Node):
         self.cl_to_r(node)
         self.cr_to_l(node)
@@ -75,80 +77,140 @@ class Graph:
         graph.process_queue()
 
     def cl_to_r(self, node: Node):
-        if (node.mr >= node.cr + 1 or node.mr == 0) and node.boat == False and node.cl > 0:
-            new_node = Node(node.cl - 1, node.cr + 1, node.ml, node.mr, True)
+        if node.boat is False and node.cl > 0:
+            if node.mr >= node.cr + 1 or node.mr == 0:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl - 1, node.cr + 1, node.ml, node.mr, True, color, is_end)
             if not self.nodes_contain(new_node):
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "1")):
                 self.edges.append(Edge(node, new_node, "1"))
 
     def cr_to_l(self, node: Node):
-        if (node.ml >= node.cl + 1 or node.ml == 0) and node.boat == True and node.cr > 0:
-            new_node = Node(node.cl + 1, node.cr - 1, node.ml, node.mr, False)
+        if node.boat is True and node.cr > 0:
+            if node.ml >= node.cl + 1 or node.ml == 0:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl + 1, node.cr - 1, node.ml, node.mr, False, color, is_end)
             if not self.nodes_contain(new_node):
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "2")):
                 self.edges.append(Edge(node, new_node, "2"))
 
     def ml_to_r(self, node: Node):
-        if (node.ml - 1 >= node.cl or node.ml == 1) and node.boat == False and node.mr + 1 >= node.cr and node.ml > 0:
-            new_node = Node(node.cl, node.cr, node.ml - 1, node.mr + 1, True)
+        if node.boat is False and node.mr + 1 >= node.cr and node.ml > 0:
+            if node.ml - 1 >= node.cl or node.ml == 1:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl, node.cr, node.ml - 1, node.mr + 1, True, color, is_end)
             if new_node not in self.nodes:
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "3")):
                 self.edges.append(Edge(node, new_node, "3"))
 
     def mr_to_l(self, node: Node):
-        if (node.mr - 1 >= node.cr or node.mr == 1) and node.boat == True and node.ml + 1 >= node.cl and node.mr > 0:
-            new_node = Node(node.cl, node.cr, node.ml + 1, node.mr - 1, False)
+        if node.boat is True and node.ml + 1 >= node.cl and node.mr > 0:
+            if node.mr - 1 >= node.cr or node.mr == 1:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl, node.cr, node.ml + 1, node.mr - 1, False, color, is_end)
             if new_node not in self.nodes:
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "4")):
                 self.edges.append(Edge(node, new_node, "4"))
 
     def kl2_to_r(self, node: Node):
-        if (node.mr >= node.cr + 2 or node.mr == 0) and node.boat == False and node.cl > 1:
-            new_node = Node(node.cl - 2, node.cr + 2, node.ml, node.mr, True)
+        if node.boat is False and node.cl > 1:
+            if node.mr >= node.cr + 2 or node.mr == 0:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl - 2, node.cr + 2, node.ml, node.mr, True, color, is_end)
             if not self.nodes_contain(new_node):
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "5")):
                 self.edges.append(Edge(node, new_node, "5"))
 
     def kr2_to_l(self, node: Node):
-        if (node.ml >= node.cl + 2 or node.ml == 0) and node.boat == True and node.cr > 1:
-            new_node = Node(node.cl + 2, node.cr - 2, node.ml, node.mr, False)
+        if node.boat is True and node.cr > 1:
+            if node.ml >= node.cl + 2 or node.ml == 0:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl + 2, node.cr - 2, node.ml, node.mr, False, color, is_end)
             if not self.nodes_contain(new_node):
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "6")):
                 self.edges.append(Edge(node, new_node, "6"))
 
     def ml2_to_r(self, node: Node):
-        if (node.ml - 2 >= node.cl or node.ml == 2) and node.boat == False and node.mr + 2 >= node.cr and node.ml > 1:
-            new_node = Node(node.cl, node.cr, node.ml - 2, node.mr + 2, True)
+        if node.boat == False and node.mr + 2 >= node.cr and node.ml > 1:
+            if node.ml - 2 >= node.cl or node.ml == 2:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl, node.cr, node.ml - 2, node.mr + 2, True, color, is_end)
             if new_node not in self.nodes:
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "7")):
                 self.edges.append(Edge(node, new_node, "7"))
 
     def mr2_to_l(self, node: Node):
-        if (node.mr - 2 >= node.cr or node.mr == 2) and node.boat == True and node.ml + 2 >= node.cl and node.mr > 1:
-            new_node = Node(node.cl, node.cr, node.ml + 2, node.mr - 2, False)
+        if node.boat == True and node.ml + 2 >= node.cl and node.mr > 1:
+            if node.mr - 2 >= node.cr or node.mr == 2:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl, node.cr, node.ml + 2, node.mr - 2, False, color, is_end)
             if new_node not in self.nodes:
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "8")):
                 self.edges.append(Edge(node, new_node, "8"))
 
     def kl_ml_to_r(self, node: Node):
-        if node.boat == False and node.cl > 0 and node.ml > 0 and node.cr+1 <= node.mr+1:
-            new_node = Node(node.cl - 1, node.cr + 1, node.ml - 1, node.mr + 1, True)
+        if node.boat == False and node.cl > 0 and node.ml > 0:
+            if node.cr + 1 <= node.mr + 1:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl - 1, node.cr + 1, node.ml - 1, node.mr + 1, True, color, is_end)
             if not self.nodes_contain(new_node):
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "9")):
                 self.edges.append(Edge(node, new_node, "9"))
 
     def kr_mr_to_l(self, node: Node):
-        if node.boat == True and node.cr > 0 and node.mr > 0 and node.cl+1 <= node.ml+1:
-            new_node = Node(node.cl + 1, node.cr - 1, node.ml + 1, node.mr - 1, False)
+        if node.boat == True and node.cr > 0 and node.mr > 0:
+            if node.cl + 1 <= node.ml + 1:
+                color = "#138025"
+                is_end = False
+            else:
+                color = "#801313"
+                is_end = True
+            new_node = Node(node.cl + 1, node.cr - 1, node.ml + 1, node.mr - 1, False, color, is_end)
             if not self.nodes_contain(new_node):
                 self.add_node(new_node)
             if not self.edges_contain(Edge(node, new_node, "10")):
@@ -160,17 +222,19 @@ class Graph:
 
 
 graph = Graph()
-graph.add_node(Node(3, 0, 3, 0, False))
+graph.add_node(Node(3, 0, 3, 0, False, '#136180', False))
 graph.process_queue()
 graph.print_graph()
-network = Network(directed=True, height="750px", width="100%", bgcolor="#222222", font_color="white")
+network = Network(directed=True, width="100%", bgcolor="#222222", font_color="white", height="99vh")
 for nodee in graph.nodes:
-    network.add_node(nodee.index, label=str(nodee.get_label()), title=str(nodee.index))
+    if nodee.cl==0 and nodee.ml==0 and nodee.cr==3 and nodee.mr==3 and nodee.boat==True:
+        nodee.color='#136180'
+    network.add_node(nodee.index, label=str(nodee.get_label()), title=str(nodee.index), color=nodee.color)
 for edgee in graph.edges:
     network.add_edge(edgee.node1.index, edgee.node2.index, label=str(edgee.instruction))
 
 network.set_edge_smooth('dynamic')
 network.toggle_physics(True)
-network.show_buttons(filter_=['physics'])
-network.barnes_hut(gravity=-3000)
+#network.show_buttons(filter_=['physics'])
+network.barnes_hut(gravity=-3000, central_gravity=0, spring_strength=0.5, damping=1, overlap=1)
 network.show("index.html", local=True, notebook=False)
